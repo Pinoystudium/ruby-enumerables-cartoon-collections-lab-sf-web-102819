479311a619f8deaaf083c1ab7e7793840873f342
Michael Espina <michael_espina@yahoo.com>
2019-10-27 04:35:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,6 +26,10 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
+
+
+
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
